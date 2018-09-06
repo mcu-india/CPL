3ea943427612011eb5285f9db3421ac494949eb9
Ankit soni <aryanjack98@gmail.com>
2018-09-05 21:45:48 -0700

    this is Assignment 1 .A "hello world " program

---
+++ Basics/ankitsoni_bca_2016/Assignment1/Hello.c
@@ -0,0 +1,15 @@
+/* Prog name- Hello world.c
+	Programmer - Ankit soni
+	Date- 		6-9-18
+*/
+
+
+#include<stdio.h>		//printf function are stored in stdio.h header file thats why we include this
+int main()			// int is return type of main function and excution of prog start from main function 
+
+
+
+{
+	printf("hello world");	//printf is function that give a output on the screen any string in double qoutes are print as it is on the screen
+	return 0;				//it return 0 when prog run successfully
+}
