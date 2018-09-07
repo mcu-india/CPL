99fc4f7e4cb3f81fba074453145fcc9f2ad5a4e9
Ismaeel Siddiqui <ismaeelshn@gmail.com>
2018-09-07 19:13:43 +0530

    correction

---
+++ Basics/Ismaeel Siddiqui_MCA_2018/Hello.c
@@ -0,0 +1,15 @@
+// including header files
+ #include<stdio.h>
+
+//Starting main function
+
+void main()
+{  
+	// Using output function to print hello world
+	
+	printf("Hello World!");
+
+
+}
+
+
